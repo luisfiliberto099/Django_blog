c2c43c600000d888a8d93ea38cac47f0d38cc978
luisfiliberto099 <56283373+luisfiliberto099@users.noreply.github.com>
2022-10-03 10:47:54 -0700

    se corrigio las rutas de los archivos estaticos

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from apps.blog.models import *
+
 
 def home(request):
-    return render(request, 'index.html')
+    post = Post.objects.filter(estado=True)
+    print(post)
+    return render(request, 'index.html', {'post': post})
+
+
+def generales(request):
+    posts = Post.objects.filter(Categoria.objects.get(nombre='General'))
+    return render(request, 'generales.html', {'posts': posts})
