d5d30f3c9082e34f40ed20f44ad0623c511d65ed
luisfiliberto099 <56283373+luisfiliberto099@users.noreply.github.com>
2022-10-05 09:54:38 -0400

    arreglo del index.html

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -6,8 +6,3 @@ def home(request):
     post = Post.objects.filter(estado=True)
     print(post)
     return render(request, 'index.html', {'post': post})
-
-
-def generales(request):
-    posts = Post.objects.filter(Categoria.objects.get(nombre='General'))
-    return render(request, 'generales.html', {'posts': posts})
